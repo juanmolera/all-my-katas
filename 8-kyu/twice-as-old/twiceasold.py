730f2c693739d6327bcbb59f05e4b2bda0a52763
juanmolera <juan.molera@gmail.com>
2023-06-06 11:13:13 +0200

    .

---
+++ 8-kyu/twice-as-old/twiceasold.py
@@ -0,0 +1,5 @@
+def twice_as_old(dad_years_old, son_years_old):
+    if dad_years_old - son_years_old * 2 > 0:
+        return dad_years_old - son_years_old * 2
+    else:
+        return -(dad_years_old - son_years_old * 2)
